1d9e2cae6c0b214d30e20b0b8b08f295a58e4f7e
Jenskid de Gracia <jpdegracia14@gmail.com>
2025-03-20 16:54:43 +0800

    add backend server

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 const corsOptions = {
     origin: 
 
-    ['http://localhost:8000','https://sample-blog-client.vercel.app/', 'https://sample-blog-5.onrender.com/'], 
+    ['https://sample-blog-client.vercel.app/', 'https://sample-blog-5.onrender.com/'], 
 
     credentials: true,  // Allow cookies and headers
     optionsSuccessStatus: 200 // For legacy browser support
